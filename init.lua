2c732ac2f98561b275dd58879333cdb2989fb7d0
mkutay <me@mkutay.dev>
2023-07-15 12:20:50 +0300

    mocha

--- init.lua
+++ init.lua
@@ -18,7 +18,7 @@ return {
   },
 
   -- Set colorscheme to use
-  colorscheme = "catppuccin-latte",
+  colorscheme = "catppuccin-mocha",
 
   -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
   diagnostics = {
